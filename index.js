a89b402ca8205ecbf2db0bd1b48ce05b25bfcec7
Shawn Engmann <86177860+Shawn-E@users.noreply.github.com>
2023-03-18 13:43:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,42 @@
-// Imports
-const axios = require('axios');
+const { create } = require('axios');
 
-// Configurations
-let config = {
-    "apiURL": "https://oocradio.com/api/1.1/wf",
-    "docsURL": "https://developer.oocradio.com/docs/intro"
-};
-
-// Functions
-async function getCurrentSong() {
-    let request = await axios({
-        method: 'get',
-        url: `${config.apiURL}/get_song`,
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
+class OOCRaido {
+    constructor({ version }, {
+        baseURL = `https://oocradio.com/api/${version}/wf` // Base URL For OOC Radio Api
+    } = {}) {
+        this.client = create({
+            baseURL, headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+    }
+    async getCurrentSong() {
+        try {
+            const { data } = await this.client.get(`/get_song`)
+            return data
+        } catch (error) {
+            console.log(error.message)
         }
-    });
-    return request.data.response;
-};
+    }
 
-async function nextLive() {
-    let request = await axios({
-        method: 'get',
-        url: `${config.apiURL}/next_live`,
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
+    async nextLive() {
+        try {
+            const { data } = await this.client.get(`/next_live`)
+            return data
+        } catch (error) {
+            console.log(error.message)
         }
-    });
-    return request.data.response;
-};
+    }
 
-async function getLiveData() {
-    let request = await axios({
-        method: 'get',
-        url: `${config.apiURL}/get_live_data`,
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
+    async getLiveData() {
+        try {
+            const { data } = await this.client.get(`/get_live_data`)
+            return data
+        } catch (error) {
+            console.log(error.message)
         }
-    });
-    return request.data.response;
-};
+    }
+}
 
-// Exports
-module.exports = {
-    getCurrentSong: getCurrentSong,
-    nextLive: nextLive,
-    getLiveData: getLiveData
-};
+module.exports = OOCRaido;
